aacb2e3848ad7ccf38e7ba0caa8f0440c72931e9
Anca200 <anca2k17@gmail.com>
2024-06-09 20:30:58 +0300

    update

--- src/pages/Projects/Project_1.jsx
+++ src/pages/Projects/Project_1.jsx
@@ -13,8 +13,8 @@ const items = [
     title: "E-commerce",
     img: e_commerce,
     desc: "This project was my initial foray into building an e-commerce website with React. Through this experience, I gained valuable insights into the architecture and development process of an e-commerce platform. I had fun building this project",
-    live: <a href="https://euphonious-lokum-728f4a.netlify.app/" target="_blank">Live</a>,
-    code: <a href="https://github.com/Anca200/E-commerce.git" target="_blank">Code</a>
+    live: "https://euphonious-lokum-728f4a.netlify.app/",
+    code:"https://github.com/Anca200/E-commerce.git" 
   
   },
   {
@@ -22,24 +22,24 @@ const items = [
     title: "Restaurant Landing Page",
     img:restaurant,
     desc: "This project provided a comprehensive introduction to React and reinforced the importance of component-based architecture in modern web development.The use of props allowed for efficient data transfer between components, facilitating a seamless and dynamic user experience.  ",
-    live: <a href="https://food-app-2024.netlify.app/" target="_blank">Live</a>,
-    code: <a href="https://github.com/Anca200/Restaurant-Landing-Page.git" target="_blank">Code</a>
+    live:"https://food-app-2024.netlify.app/",
+    code: "https://github.com/Anca200/Restaurant-Landing-Page.git"
   },
   {
     id:3,
     title: "Movie Search",
     img: movie,
     desc: "This project provided me with a foundational understanding of API integration and its implementation within a React application. For the first time, I learned how to connect to an external API and dynamically fetch and display data. The application features a search bar where users can input the name of a movie. Upon submission, the relevant movie data is retrieved from the API and rendered on the page.",
-    live: <a href="https://movie-app-anca200.netlify.app/" target="_blank">Live</a>,
-    code: <a href="https://github.com/Anca200/Movie-app.git" target="_blank">Code</a>
+    live: "https://movie-app-anca200.netlify.app/" ,
+    code: "https://github.com/Anca200/Movie-app.git"
   },
   {
     id:4,
     title: "Pong Game",
     img: pong,
     desc: "This project encompasses the development of a two-player Pong game using JavaScript. The creation of this game was an engaging experience that significantly enhanced my understanding of JavaScript. In this game, one player utilizes the 'WASD' keys for control, while the other player uses the arrow keys.",
-    live: <a href="https://anca200.github.io/Pong-Game/" target="_blank">Live</a>,
-    code: <a href="https://github.com/Anca200/Pong-Game.git" target="_blank">Code</a>
+    live: "https://anca200.github.io/Pong-Game/",
+    code:"https://github.com/Anca200/Pong-Game.git"
   },
 ];
 
@@ -54,8 +54,8 @@ const Single = ({item}) => {
      <div className='text-wrapper'> 
      <h2>{item.title}</h2>
      <p>{item.desc}</p>
-     <button className='live'>{item.live}</button>
-     <button className='code'>{item.code}</button>
+     <a href={item.live} target="_blank"><button className='live'>Live</button></a>
+     <a href={item.code} target="_blank"><button className='live'>Code</button></a>
      </div>
      </div>
     </section>
