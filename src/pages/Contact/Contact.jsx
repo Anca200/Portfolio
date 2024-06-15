9f01c2082e3f2231fe3ecb2ea69de781ba997d46
Anca200 <anca2k17@gmail.com>
2024-06-16 02:15:30 +0300

    update

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -52,12 +52,10 @@ const sendEmail = (e) => {
  <h1>Contact me</h1>
 <div className='item'>
  <h2>Email:</h2>
- <hr/>
  <span>anca2k17@gmail.com</span>
 </div>
 <div className='item'>
  <h2>Download CV:</h2>
- <hr/>
  <a href={resume}><button>Download</button></a>
 
 
@@ -69,7 +67,7 @@ const sendEmail = (e) => {
   initial={{opacity:1}}
   whileInView={{opacity:0}}
   transition={{delay:1.5,duration:1}}>
- <svg xmlns="http://www.w3.org/2000/svg" fill=" #38598b" viewBox="0 0 512 512"><path strokeWidth={0.2}  d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg>
+ <svg xmlns="http://www.w3.org/2000/svg" fill="#111132" viewBox="0 0 512 512"><path strokeWidth={0.2}  d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg>
  </motion.div>
 
 <motion.form 
