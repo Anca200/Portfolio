dedd0ce0714dcddd47f232d80a02794099131ca0
Anca200 <anca2k17@gmail.com>
2024-06-16 00:59:44 +0300

    update

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,5 @@
 import "./index.css";
 import Hero from "./pages/Hero/Hero";
-import Parallax from "./pages/Parallax/Parallax";
 import About from "./pages/About/About";
 import Project_1 from "./pages/Projects/Project_1";
 
@@ -11,9 +10,7 @@ function Home() {
   return (
     <>
       <section id="Home"><Hero/></section>
-      <section id="About"><Parallax type="about"/></section>
       <section id="About"><About/></section>
-      <section id="Projects"><Parallax type="projects"/></section>
       <Project_1/>
       <section id="Contact"><Contact/></section>
 
