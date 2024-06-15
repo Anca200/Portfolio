5b0230ae80290dc45969c2b253e6292adaffe16a
Anca200 <anca2k17@gmail.com>
2024-06-16 02:46:34 +0300

    update

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -44,7 +44,7 @@ const sendEmail = (e) => {
 };
   
   return (
-    <div className='contact'
+    <motion.div className='contact'
     variants={variants}
     initial ="initial"
     whileInView="animate">
@@ -63,24 +63,31 @@ const sendEmail = (e) => {
  </div>
 
  <div className='form-container'> 
- <div className='svg-form'>
- 
- </div>
+ <motion.div className='svg-form'
+  initial={{opacity:1}}
+  whileInView={{opacity:0}}
+  transition={{delay:1.5,duration:1}}>
+ <svg xmlns="http://www.w3.org/2000/svg" fill="#111132" viewBox="0 0 512 512"><path strokeWidth={0.2}  d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg>
+ </motion.div>
 
-<form >
+<motion.form 
+ref={formRef}
+onSubmit={sendEmail}
+ initial={{opacity:0}}
+ whileInView={{opacity:1}}
+ transition={{delay: 2.5,duration:1}}>
 
-
-  <input type="text" required placeholder='Name' name="name"/>
+  <input type="text" required placeholder='Name' name="name" className='p-5'/>
   <input type="email" required placeholder='Email' name= "email"/>
   <textarea rows={8} placeholder='Message' name="message"/>
   
   <button type="submit">Submit</button>
   {error && "Error"}
   {success && "Success"}
-</form>
+</motion.form>
 
  </div>
-    </div>
+    </motion.div>
 
   )
 }
