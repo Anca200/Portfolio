5dc2bcf6994a34a7640b00969697755ceeff5bac
Anca200 <anca2k17@gmail.com>
2024-06-16 02:34:42 +0300

    Update Contact.jsx

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -44,7 +44,7 @@ const sendEmail = (e) => {
 };
   
   return (
-    <motion.div className='contact'
+    <div className='contact'
     variants={variants}
     initial ="initial"
     whileInView="animate">
@@ -63,19 +63,15 @@ const sendEmail = (e) => {
  </div>
 
  <div className='form-container'> 
- <motion.div className='svg-form'
+ <div className='svg-form'
   initial={{opacity:1}}
   whileInView={{opacity:0}}
   transition={{delay:1.5,duration:1}}>
- <svg xmlns="http://www.w3.org/2000/svg" fill="#111132" viewBox="0 0 512 512"><path strokeWidth={0.2}  d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg>
- </motion.div>
+ 
+ </div>
 
-<motion.form 
-ref={formRef}
-onSubmit={sendEmail}
- initial={{opacity:0}}
- whileInView={{opacity:1}}
- transition={{delay: 2.5,duration:1}}>
+<form >
+
 
   <input type="text" required placeholder='Name' name="name"/>
   <input type="email" required placeholder='Email' name= "email"/>
@@ -84,10 +80,10 @@ onSubmit={sendEmail}
   <button type="submit">Submit</button>
   {error && "Error"}
   {success && "Success"}
-</motion.form>
+</form>
 
  </div>
-    </motion.div>
+    </div>
 
   )
 }
