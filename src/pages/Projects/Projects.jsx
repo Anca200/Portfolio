31e510e7499da7febd4f752cbdb4ef687bbc928d
Anca200 <anca2k17@gmail.com>
2024-06-25 10:05:22 +0300

    update

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -12,23 +12,23 @@ const items = [
     id:1,
     title: "Netflix Clone",
     img: netflix,
-    desc: "This project provided me with a foundational understanding of API integration and its implementation within a React application. For the first time, I learned how to connect to an external API and dynamically fetch and display data. The application features a search bar where users can input the name of a movie. Upon submission, the relevant movie data is retrieved from the API and rendered on the page.",
+    desc: "For this project i used the TMDB API to fetch and display data. For the registration system i used Firebase to Sign Up and Log In and Firestore Database to store the movies added to favorite. The style was made usind Tailwind  ",
     live: "https://golden-dolphin-f31e35.netlify.app/" ,
     code: "https://github.com/Anca200/netflix-vite.git"
   },
   {
     id:2,
-    title: "Crypto Search Website",
+    title: "Crypto Search API",
     img:crypto,
-    desc: "This project provided a comprehensive introduction to React and reinforced the importance of component-based architecture in modern web development.The use of props allowed for efficient data transfer between components, facilitating a seamless and dynamic user experience.  ",
+    desc: "This project was made using CoinGecko API for fetching the data and changing the currency from USD to Euro. Typing in the search bar will display the searched coin, additionally by selecting a coin, a new page will open with a chart and data. The style was made with Tailwind. Firesbase authentication will be added in the future",
     live:"https://peaceful-pixie-d78816.netlify.app/",
     code: "https://github.com/Anca200/CRYPTO.gitt"
   },
   {
     id:3,
-    title: "E-commerce",
+    title: "E-commerce Website",
     img: e_commerce,
-    desc: "This project was my initial foray into building an e-commerce website with React. Through this experience, I gained valuable insights into the architecture and development process of an e-commerce platform. I had fun building this project",
+    desc: "This project was my first advanced E-commerce website. The data was fetched from a json file and displayed using the map method. The products can be stored to cart, updating it based on the number added and displayed in a special page, each product contains a dedicated page as well. The style was made with CSS ",
     live: "https://euphonious-lokum-728f4a.netlify.app/",
     code:"https://github.com/Anca200/E-commerce.git" 
   
@@ -37,7 +37,7 @@ const items = [
     id:4,
     title: "VirtualR Website",
     img: virtual,
-    desc: "This project encompasses the development of a two-player Pong game using JavaScript. The creation of this game was an engaging experience that significantly enhanced my understanding of JavaScript. In this game, one player utilizes the 'WASD' keys for control, while the other player uses the arrow keys.",
+    desc: "This is a simple landing page. I focused on the page structure and design using Tailwind",
     live: "https://gorgeous-babka-fa922f.netlify.app/",
     code:"https://github.com/Anca200/Website-VirtualR.git"
   }
@@ -55,6 +55,7 @@ const Single = ({item}) => {
       </div> 
        <div className='text-wrapper'> 
      <h2>{item.title}</h2>
+     <p>{item.desc}</p>
 
      <a href={item.live} target="_blank"><button className='live'>Live</button></a>
      <a href={item.code} target="_blank"><button className='live'>Code</button></a>
